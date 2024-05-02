a360a8f9d1b5edc56a75a7a47b99857c4ce8d3d8
Genlocker <ogunrukuseun@gmail.com>
2024-05-02 15:06:44 +0100

    new changes

---
+++ src/components/InfoSection/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Button } from "../ButtonElement";
+import {
+  Column2,
+  Img,
+  ImgWrap,
+  InfoContainer,
+  InfoRow,
+  InfoWrapper,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrapper,
+} from "./InfoElements";
+
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  img,
+  alt,
+  buttonLabel,
+}) => {
+  return (
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InfoWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headline}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
+                <BtnWrapper>
+                  <Button to="home">{buttonLabel}</Button>
+                </BtnWrapper>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  );
+};
+
+export default InfoSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
+import React from "react";
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const [isOpen, setIsOpen] = useState(false) 
-
-const toggle = () => {
-  setIsOpen(!isOpen)
-}
-
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
-      <Sidebar isOpen = {isOpen} toggle ={toggle} />
-      <Navbar toggle={toggle}/>
-      <HeroSection/>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
