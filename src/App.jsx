7f8f28f060311505f84e8e5466507f9870ac573d
Genlocker <ogunrukuseun@gmail.com>
2024-05-03 14:17:19 +0100

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './pages';
-
+import "./App.css";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" component={Home} />
+    //     <Route path="/signin" component={SigninPage} />
+    //   </Switch>
+    // </Router>
     <Router>
-      <Home/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
     </Router>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
